f63dae1f9fbe8b1dea175e623a8b0d67ead21489
Iconians <claytoncripe@gmail.com>
2023-02-23 11:23:19 -0800

    added styles to product cards

--- src/Components/ProductHolder/ProductHolder.tsx
+++ src/Components/ProductHolder/ProductHolder.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./ProductHolder.css";
 
 interface props {
@@ -7,13 +7,22 @@ interface props {
 }
 
 export const ProductHolder = ({ img, id }: props) => {
+  const navigate = useNavigate();
+  const navigateToProductPage = () => {
+    navigate("/ProductPage", { state: { productId: id } });
+  };
   return (
-    <div className="product-holder-wrapper" key={id}>
+    <div
+      className="product-holder-wrapper"
+      key={id}
+      onClick={navigateToProductPage}
+    >
       <div className="img-wrapper">
         <img src={img} alt="" />
       </div>
       <div>
         <Link
+          className="link-wrapper"
           to="/ProductPage"
           state={{
             productId: id,
